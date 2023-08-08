8aeec0ecdc800e00b6a84abb33d72b093e65461b
Mobin Vaziri <mobin.vaziri@inrs.ca>
2023-08-08 16:00:41 -0400

    unroll, dsp, and allocation added.

--- Hardware Phase/CPP generator.py
+++ Hardware Phase/CPP generator.py
@@ -1,6 +1,50 @@
-def create_cpp_file(filename, inp_sample:int):
-    cpp_code = f"""
-#include <iostream>
+
+
+def create_cpp_file(filename, inp_sample: int, out_n: int, one_mac: bool, no_dsp: bool):
+    
+	output_reg = ""
+	feedback = ""
+
+	factor_1 = 8
+	factor_2 = 3
+	factor_3 = 3
+	factor_4 = 8
+
+	if (one_mac == True):
+
+		factor_1 = factor_2 = factor_3 = factor_4 = 1
+
+		alloc_1 = "#pragma HLS ALLOCATION operation instances=fmul limit=1"
+		alloc_2 = "#pragma HLS ALLOCATION operation instances=fadd limit=1"
+	
+	else:
+		alloc_1 = ""
+		alloc_2 = ""
+
+	if (no_dsp == True):
+		
+		bind_op1 = "#pragma HLS BIND_OP variable=acc_1 op=fadd impl=fabric"
+		bind_op2 = "#pragma HLS BIND_OP variable=acc_1 op=fmul impl=fabric"
+		bind_op3 = "#pragma HLS BIND_OP variable=acc_2 op=fadd impl=fabric"
+		bind_op4 = "#pragma HLS BIND_OP variable=acc_2 op=fmul impl=fabric"
+	
+	else:
+
+		bind_op1 = ""
+		bind_op2 = ""
+		bind_op3 = ""
+		bind_op4 = ""
+		
+
+
+
+	for i in range (out_n):
+    
+		output_reg = output_reg + f"Y[{i}] = y_reg[{i}] \n\t\t"
+		feedback = feedback + f"x_reg[{i}] = y_reg[{i}] \n\t\t"
+	
+	cpp_code = f"""
+#include "HENNC.h"
 
 void ChaoticOscillator(stream &X, stream &W1, stream &W2, stream &B1, stream &B2, data_t Y[out_n])
 {{
@@ -78,27 +122,90 @@ void ChaoticOscillator(stream &X, stream &W1, stream &W2, stream &B1, stream &B2
 
 		mlp_core (x_reg, w1_reg, w2_reg, b1_reg, b2_reg, y_reg);
 
-		Y[0] = y_reg[0];
-		Y[1] = y_reg[1];
-		Y[2] = y_reg[2];
-
-		x_reg[0] = y_reg[0];
-		x_reg[1] = y_reg[1];
-		x_reg[2] = y_reg[2];
-
+        {output_reg}
+        {feedback}
 
 		sample++;
 	}}
 
 
 }}
+
+void mlp_core(data_t mlp_in[inp_n], data_t w1_in[inp_n][hid_n], data_t w2_in[hid_n][out_n], data_t b1_in[hid_n], data_t b2_in[out_n], data_t mlp_out[out_n])
+
+{{
+
+{alloc_1}
+{alloc_2}
+
+	data_t acc_1, acc_2 = 0;
+{bind_op1}
+{bind_op2}
+{bind_op3}
+{bind_op4}
+
+	static data_t hidden_n [hid_n] = {{}};
+#pragma HLS ARRAY_RESHAPE dim=1 type=complete variable=hidden_n
+
+
+
+	for (int i = 0; i < hid_n; i++)
+	{{
+#pragma HLS UNROLL factor= {factor_1}
+
+
+		acc_1 = 0;
+
+		for (int j = 0; j < inp_n; j++)
+		{{
+#pragma HLS UNROLL factor= {factor_2}
+
+			acc_1 += mlp_in[j] * w1_in[j][i];
+		}}
+
+		acc_1 += b1_in[i];
+		hidden_n[i] = ReLU(acc_1);
+	}}
+
+	for (int i = 0; i < out_n; i++)
+	{{
+#pragma HLS UNROLL factor= {factor_3}
+
+
+
+		acc_2 = 0;
+
+		for (int j = 0; j < hid_n; j++)
+		{{
+#pragma HLS UNROLL factor= {factor_4}
+
+			acc_2 += hidden_n[j] * w2_in[j][i];
+		}}
+
+		acc_2 += b2_in[i];
+		mlp_out[i] = acc_2;
+	}}
+}}
+
+
+
+data_t ReLU (data_t inp_relu)
+
+{{
+#pragma HLS INLINE
+	data_t out_relu;
+
+	out_relu = (hls::signbit(inp_relu) == 0) ? (inp_relu) : (0);
+
+	return (out_relu);
+}}
 """
 
-    with open(filename, "w") as file:
-        file.write(cpp_code)
+	with open(filename, "w") as file:
+		file.write(cpp_code)
 
 if __name__ == "__main__":
 
     cpp_filename = "Hardware Phase\HENNC.cpp"
-    create_cpp_file(cpp_filename, 100)
+    create_cpp_file(cpp_filename, 100, 3, 0, 0)
     print(f"{cpp_filename} has been created.")
--- Hardware Phase/HENNC.cpp
+++ Hardware Phase/HENNC.cpp
@@ -1,5 +1,5 @@
 
-#include <iostream>
+#include "HENNC.h"
 
 void ChaoticOscillator(stream &X, stream &W1, stream &W2, stream &B1, stream &B2, data_t Y[out_n])
 {
@@ -77,17 +77,86 @@ void ChaoticOscillator(stream &X, stream &W1, stream &W2, stream &B1, stream &B2
 
 		mlp_core (x_reg, w1_reg, w2_reg, b1_reg, b2_reg, y_reg);
 
-		Y[0] = y_reg[0];
-		Y[1] = y_reg[1];
-		Y[2] = y_reg[2];
-
-		x_reg[0] = y_reg[0];
-		x_reg[1] = y_reg[1];
-		x_reg[2] = y_reg[2];
-
+        Y[0] = y_reg[0] 
+		Y[1] = y_reg[1] 
+		Y[2] = y_reg[2] 
+		
+        x_reg[0] = y_reg[0] 
+		x_reg[1] = y_reg[1] 
+		x_reg[2] = y_reg[2] 
+		
 
 		sample++;
 	}
 
 
+}
+
+void mlp_core(data_t mlp_in[inp_n], data_t w1_in[inp_n][hid_n], data_t w2_in[hid_n][out_n], data_t b1_in[hid_n], data_t b2_in[out_n], data_t mlp_out[out_n])
+
+{
+
+
+
+
+	data_t acc_1, acc_2 = 0;
+
+
+
+
+
+	static data_t hidden_n [hid_n] = {};
+#pragma HLS ARRAY_RESHAPE dim=1 type=complete variable=hidden_n
+
+
+
+	for (int i = 0; i < hid_n; i++)
+	{
+#pragma HLS UNROLL factor= 8
+
+
+		acc_1 = 0;
+
+		for (int j = 0; j < inp_n; j++)
+		{
+#pragma HLS UNROLL factor= 3
+
+			acc_1 += mlp_in[j] * w1_in[j][i];
+		}
+
+		acc_1 += b1_in[i];
+		hidden_n[i] = ReLU(acc_1);
+	}
+
+	for (int i = 0; i < out_n; i++)
+	{
+#pragma HLS UNROLL factor= 3
+
+
+
+		acc_2 = 0;
+
+		for (int j = 0; j < hid_n; j++)
+		{
+#pragma HLS UNROLL factor= 8
+
+			acc_2 += hidden_n[j] * w2_in[j][i];
+		}
+
+		acc_2 += b2_in[i];
+		mlp_out[i] = acc_2;
+	}
+}
+
+
+
+data_t ReLU (data_t inp_relu)
+
+{
+#pragma HLS INLINE
+	data_t out_relu;
+
+	out_relu = (hls::signbit(inp_relu) == 0) ? (inp_relu) : (0);
+
+	return (out_relu);
 }
