24c9074f3c27a98673ec02b54a802eb8c28215b2
shervin-vakili <shervin.vakili@inrs.ca>
2023-08-09 20:45:00 -0400

    HENNC_Generator is now the top function in the hardware phase. -g flag generates low-cost single-MAC solution. -p gererates the highest-performance solution. -a shows all available candidate sulutions in detail. -i show all options and asks the user to select one and generates the selected solution

--- Hardware Phase/Analyzer.py
+++ Hardware Phase/Analyzer.py
@@ -1,19 +1,16 @@
 
 from HLS_Generator import *
 
-def TOP (inp_ns, hid_ns, out_ns, sample, x_0, y_0, z_0, one_mac: bool, no_dsp: bool, solution: bool, mode: str):
+def TOP (inp_ns, hid_ns, out_ns, sample, x_0, y_0, z_0, one_mac: bool, no_dsp: bool,  mode: str):
 
     n_factor = 4
     factor_1 = factor_2 = factor_3 = factor_4 = 1
-
     factor_1_list = list()
     factor_2_list = [inp_ns]
     
-    
-    
     solutions = [[1,1,1,1]]
-    
-    if (solution == True and mode == ""):
+
+    if (mode == 'interactive' or mode == 'analyze'):
 
         for idx_1 in reversed(range(2, hid_ns + 1)):
 
@@ -21,7 +18,6 @@ def TOP (inp_ns, hid_ns, out_ns, sample, x_0, y_0, z_0, one_mac: bool, no_dsp: b
 
                 factor_1_list.append((idx_1)) 
 
-        
         for i in factor_1_list:
             
             for j in factor_2_list:
@@ -30,52 +26,53 @@ def TOP (inp_ns, hid_ns, out_ns, sample, x_0, y_0, z_0, one_mac: bool, no_dsp: b
 
         print(solutions)
 
+        # Estimate Function
 
-    elif (solution == False and mode == "Performance"):
+    if mode == 'interactive':
 
+        print(f"You have {len(solutions)} solutions.\n")
+
+        for i in range(len(solutions)):
+
+            print(f"Solution {i} -> Estimated Area:        Estimated Latency:       \n")
+
+        key = int(input("Which solution you prefer ? \n"))
+
+        factor_1 = solutions[key][0]
+        factor_2 = solutions[key][1]
+        factor_3 = solutions[key][2]
+        factor_4 = solutions[key][3]
+
+    elif mode == 'analyze':
+        print(f"There are {len(solutions)} solutions available:\n")
+        for i in range(len(solutions)):
+            print(f"Solution {i} -> Estimated Area:        Estimated Latency:       \n")
+        print("drawing the plot")
+        # Draw the plot
+    elif mode == 'generate_p': # Generate with performance as the high priority
         factor_1 = hid_ns
         factor_2 = inp_ns
         factor_3 = out_ns
         factor_4 = hid_ns
 
-    elif (solution == False and mode == "Cost"):
-
+    elif (mode == "generate_c"): # Generate with cost as the high priority
         factor_1 = 1
         factor_2 = 1
         factor_3 = 1
         factor_4 = 1
-
-
     else:
-
-        print("Hi.")
-        pass
-
+        print("Error: wrong mode argument in Top function")
     
-    # Estimate Function
-
-    print(f"You have {len(solutions)} solutions.\n")
-
-    for i in range(len(solutions)):
-
-        print(f"Solution {i} -> Estimated Area:        Estimated Latency:       \n")
-
-
-    key = int(input("Which solution you prefer ? \n"))
-
-    factor_1 = solutions[key][0]
-    factor_2 = solutions[key][1]
-    factor_3 = solutions[key][2]
-    factor_4 = solutions[key][3]
-
-    
-    create_h_file("Hardware Phase\HENNC.h", inp_ns, hid_ns, out_ns)
-    create_cpp_file("Hardware Phase\HENNC.cpp", sample, factor_1, factor_2, factor_3, factor_4, out_ns, one_mac, no_dsp)
-    create_testBench_file("Hardware Phase\ test_bench.cpp", x_0, y_0, z_0)
+    if mode == 'interactive' or mode == 'generate_c' or  mode == 'generate_p':
+        
+        create_h_file("Hardware Phase\HENNC.h", inp_ns, hid_ns, out_ns)
+        create_cpp_file("Hardware Phase\HENNC.cpp", sample, factor_1, factor_2, factor_3, factor_4, out_ns, one_mac, no_dsp)
+        create_testBench_file("Hardware Phase\ test_bench.cpp", x_0, y_0, z_0)
+        print("HLS model generated successfully")
 
 
 
-TOP(3, 8, 3, 100, 0.534522473812, 0.267261236906, 0.80178374052, 0, 0, 1, "")
+
 
 
 
---
+++ Hardware Phase/HENNC_Generator.py
@@ -0,0 +1,38 @@
+import argparse
+from Analyzer import *
+parser = argparse.ArgumentParser()
+
+inp_ns = 3
+hid_ns = 8
+out_ns = 3
+sample = 100
+x_0 = 0.534522473812
+y_0 = 0.267261236906
+z_0 = 0.80178374052
+
+#-db DATABSE -u USERNAME -p PASSWORD -size 20
+parser.add_argument("-g", "--generate", help="Generate HENNC HLS model", action='store_true')
+parser.add_argument("-p", "--performance", help="Generate high-performance HENNC HLS model", action='store_true')
+parser.add_argument("-i", "--interactive", help="Interactive generation of HENNC HLS model", action='store_true')
+parser.add_argument("-a", "--analyze", help="Analyze different solutions", action='store_true')
+
+args = parser.parse_args()
+
+print( "generate {}       interactive {}          analyze {} ".format(
+        args.generate,
+        args.interactive,
+        args.analyze
+        ))
+
+
+# default mode : generate_c
+mode = 'generate_c'
+if args.performance == True:
+    mode = 'generate_p'
+elif args.generate == True:
+    mode = 'generate_c'
+elif args.interactive == True:
+    mode = 'interactive'
+elif args.analyze == True:
+    mode = 'analyze'
+TOP(inp_ns, hid_ns, out_ns, sample, x_0, y_0, z_0, 0, 0, mode)
